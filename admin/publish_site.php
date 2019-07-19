9228cc4c8b98f4e7294d7a20b5e520cd59efd904
suny-upstate-cwt <wingmingchan64@hotmail.com>
2019-07-19 10:27:04 -0400

    Updated

--- admin/publish_site.php
+++ admin/publish_site.php
@@ -1,4 +1,8 @@
 <?php
+/*
+This program can be used to force a publish command on every page and file in a site.
+The publishAllFilesPages method is defined in the Administration class.
+*/
 require_once( 'auth_REST_SOAP.php' );
 
 use cascade_ws_AOHS      as aohs;
@@ -8,11 +12,12 @@ use cascade_ws_property  as p;
 use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
 
-$site_name = "about";
+$site_name   = "formats";
+$folder_path = "velocity/courses/exercises";
 
 try
 {
-    $admin->publishAllFilesPages( $site_name );
+    $admin->publishAllFilesPages( $site_name, $folder_path );
 }
 catch( \Exception $e ) 
 {
